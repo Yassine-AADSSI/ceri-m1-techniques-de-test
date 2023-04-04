618d5b8567dc74453962cb0f74cdba0ef2fb9d6b
AADSSI <yassine.aadssi@alumni.univ-avignon.fr>
2023-04-04 12:36:52 +0200

    Ajout des Tests Unitaires

---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -0,0 +1,33 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class IPokemonMetadataProviderTest {
+    private IPokemonMetadataProvider metaData;
+    private PokemonMetadata pokemon1;
+    private PokemonMetadata pokemon2;
+
+    @Before
+    public void setUp() throws PokedexException {
+        metaData = mock(IPokemonMetadataProvider.class);
+        pokemon1 = new PokemonMetadata(0,"Bulbizarre",126,126,90);
+        pokemon2 = new PokemonMetadata(133,"Aquali",186,168,260);
+        when(metaData.getPokemonMetadata(0)).thenReturn(pokemon1);
+        when(metaData.getPokemonMetadata(133)).thenReturn(pokemon2);
+    }
+
+    @Test
+    public void testMetadata() throws PokedexException {
+        assertEquals(metaData.getPokemonMetadata(0), pokemon1);
+        assertEquals(metaData.getPokemonMetadata(133), pokemon2);
+
+        assertEquals(metaData.getPokemonMetadata(0).getName(), pokemon1.getName());
+        assertEquals(metaData.getPokemonMetadata(133).getName(), pokemon2.getName());
+
+    }
+}
---
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -0,0 +1,37 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class IPokemonTrainerFactoryTest {
+
+    private IPokemonTrainerFactory pokemonTrainerFactory;
+    private IPokedexFactory pokedexFactory;
+    private IPokedex iPokedex;
+    private PokemonTrainer pokemonTrainer;
+
+
+    @Before
+    public void setUp() throws PokedexException {
+        pokemonTrainerFactory = mock(IPokemonTrainerFactory.class);
+        pokedexFactory = mock(IPokedexFactory.class);
+        iPokedex = mock(IPokedex.class);
+        pokemonTrainer = new PokemonTrainer("yassine", Team.MYSTIC, iPokedex);
+
+        when(pokemonTrainerFactory.createTrainer("yassine", Team.MYSTIC, pokedexFactory)).thenReturn(pokemonTrainer);
+    }
+
+    @Test
+    public void testCreateTrainer() throws PokedexException {
+        assertEquals(pokemonTrainer, pokemonTrainerFactory.createTrainer("yassine", Team.MYSTIC, pokedexFactory));
+        assertEquals("yassine", pokemonTrainer.getName());
+    }
+
+
+
+
+}
