feb95e3520c0dfcc6862bdeb16c1ad8f4b5008da
Yassine-AADSSI <yassine.aadssi@alumni.univ-avignon.fr>
2023-04-28 23:27:52 +0000

    Updates

---
+++ src/main/java/fr/univavignon/pokedex/api/Team.java
@@ -0,0 +1,22 @@
+package fr.univavignon.pokedex.api;
+
+/**
+ * Pokemon Team enumeration.
+ * @author fv
+ */
+public enum Team {
+
+	/**
+	 * The MYSTIC team.
+	 */
+	MYSTIC,
+
+	/**
+	 * The INSTINCT team.
+	 */
+	INSTINCT,
+	/**
+	 * The VALOR team.
+	 */
+	VALOR
+}
---
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -0,0 +1,34 @@
+package fr.univavignon.pokedex.api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class IPokedexFactoryTest {
+
+    private IPokedexFactory iPokedexFactory;
+    private IPokemonMetadataProvider iPokemonMetadataProvider;
+    private IPokemonFactory iPokemonFactory;
+    private IPokedex pokedex;
+
+    @Before
+    public void init() {
+
+        iPokedexFactory = mock(IPokedexFactory.class) ;
+        iPokemonMetadataProvider = mock(IPokemonMetadataProvider.class);
+        iPokemonFactory = mock(IPokemonFactory.class);
+        pokedex = mock(IPokedex.class);
+        when(iPokedexFactory.createPokedex(iPokemonMetadataProvider, iPokemonFactory)).thenReturn(pokedex);
+
+    }
+
+    @Test
+    public void createPokedex() {
+        assertEquals(iPokedexFactory.createPokedex(iPokemonMetadataProvider, iPokemonFactory), pokedex);
+    }
+
+
+}
