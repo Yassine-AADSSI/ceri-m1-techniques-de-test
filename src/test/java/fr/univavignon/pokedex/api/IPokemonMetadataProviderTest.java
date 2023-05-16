d859414d3cd875b68b2da2afd9336e7ee02b3179
AADSSI <yassine.aadssi@alumni.univ-avignon.fr>
2023-05-16 20:00:14 +0200

    Test 23

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -29,4 +29,13 @@ public class IPokemonMetadataProviderTest {
 
 
     }
+    // Test de la méthode getPokemonMetadata avec un index invalide (inférieur à 0) qui doit lever une exception
+    @Test(expected = PokedexException.class)
+    public void testGetPokemonMetadataWithInvalidIndex() throws PokedexException {
+        when(metaData.getPokemonMetadata(-1)).thenThrow(new PokedexException("L'index est invalide"));
+        metaData.getPokemonMetadata(-1);
+    }
+
+
+
 }
